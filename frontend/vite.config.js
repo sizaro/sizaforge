5adb0aa10d5a86ba9fca2c938a628abf22849f63
sizaro <spartansiza@gmail.com>
2025-12-13 04:30:21 +0300

    hosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,5 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: ["sizaforge.onrender.com"],
+  },
+
+  preview: {
+    allowedHosts: ["sizaforge.onrender.com"],
+  },
   
 })
